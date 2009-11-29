2c7ca442df3d8a896b204ea743c2102702c9a318
Rick E <codeblock@eighthbit.net>
2009-11-29 00:37:28 -0500

    Still broken, pushing so I can pull elsewhere.

--- TextFuncs.pm
+++ TextFuncs.pm
@@ -1,1 +1,21 @@
-1;
+#!/usr/bin/env perl
+use warnings;
+use strict;
+
+package Proc;
+use Text::WikiText;
+use Text::WikiText::Output::Pod;
+use WikiFuncs;
+
+sub prepare {
+   my $wikitext = shift;
+#   my $parser = Text::WikiText->new();
+#   my $proc = $parser->parse($wikitext);
+#   my $output = Text::WikiText::Output::Pod->new->dump($proc);
+   $wikitext =~ s/<!--.*?-->//sgi;
+   return $wikitext;
+}
+
+my $t = WPB::lookup($ARGV[0]);
+print prepare($t);
+
